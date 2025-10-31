2570a59edad0c0d66ed7382d408a26d06961fcb7
v.filipovich <v.filipovich@sibset-team.ru>
2025-10-31 13:21:54 +0700

    add data_encryption.py

---
+++ package.py
@@ -0,0 +1,44 @@
+from enum import Enum
+import struct
+from dataclasses import dataclass
+
+
+class PackegeError(Exception):
+    pass
+
+
+class PackegeType(Enum):
+    DATA = b'\x00'
+    PING = b'\x01'
+
+
+packege_type_dict = {
+    PackegeType.DATA: 'DATA',
+    PackegeType.PING: 'PING',
+}
+
+
+@dataclass
+class PackageData:
+    vc_type: str
+    b_data: bytes
+
+
+class Package:
+    @classmethod
+    def decode(cls, raw: bytes) -> PackageData:
+        p_type = raw[0]
+        b_data = raw[1:]
+        if p_type not in packege_type_dict:
+            raise PackegeError('type not found')
+        return PackageData(vc_type=packege_type_dict[p_type], b_data=b_data)
+
+    @classmethod
+    def ecode(cls, p_type: bytes, data: bytes) -> bytes:
+        if p_type not in packege_type_dict:
+            raise PackegeError('type not found')
+        return p_type + data
+
+
+
+
